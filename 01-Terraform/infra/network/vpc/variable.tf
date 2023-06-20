ebe11246d3bdb417f1582c9977d347682a2e47b2
kyd2506 <kyd2506@naver.com>
2023-06-20 16:10:46 +0900

    20230620

---
+++ 01-Terraform/infra/network/vpc/variable.tf
@@ -0,0 +1,15 @@
+variable "vpc_cidr" {
+    default = "10.14.0.0/16"
+}
+variable "public_subnet" {
+    type = list
+    default = ["10.14.0.0/24", "10.14.16.0/24"]
+}
+variable "private_subnet" {
+    type = list
+    default = ["10.14.64.0/24", "10.14.80.0/24"]
+}
+variable "azs" {
+    type = list
+    default = ["ap-northeast-2a", "ap-northeast-2c"]
+}
